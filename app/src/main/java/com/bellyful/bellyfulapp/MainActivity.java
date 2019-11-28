da0683e0ff24aeb64c940be9da52cc8415c35331
jasonkaminer <jasonkaminer@gmail.com>
2019-11-28 20:39:49 +1300

    Basic login UI. Disabled google analytics for now

--- app/src/main/java/com/bellyful/bellyfulapp/MainActivity.java
+++ app/src/main/java/com/bellyful/bellyfulapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.bellyful.bellyfulapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Start login activity
+        Intent startLoginIntent = new Intent(MainActivity.this, Login.class);
+        MainActivity.this.startActivity(startLoginIntent);
+
     }
 
 }
