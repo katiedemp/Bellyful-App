2873f6a49e9849435885a4afb0072870d1ee4a4d
Katie <katie.m.dempsey@gmail.com>
2019-12-09 10:11:56 +1300

    Added Nav Bar
    And appropriate icons

--- app/src/main/java/com/bellyful/bellyfulapp/MainActivity.java
+++ app/src/main/java/com/bellyful/bellyfulapp/MainActivity.java
@@ -1,19 +1,26 @@
 package com.bellyful.bellyfulapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth; //Firebase auth
+    private ActionBar toolbar;
+    BottomNavigationView bottomNavigationView; // Bottom navigation bar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +40,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //----------------------------------------
+        //Bottom Navigation Bar
+        toolbar = getSupportActionBar();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.action_new_jobs:
+                    toolbar.setTitle("New");
+                    return true;
+                case R.id.action_current_jobs:
+                    toolbar.setTitle("Current");
+                    return true;
+                case R.id.action_freezers:
+                    toolbar.setTitle("Freezers");
+                    return true;
+                case R.id.action_user:
+                    toolbar.setTitle("Profile");
+                    return true;
+            }
+            return false;
+        }
+    };
 
     private void updateUI(FirebaseUser currentUser){
         if(currentUser == null){
