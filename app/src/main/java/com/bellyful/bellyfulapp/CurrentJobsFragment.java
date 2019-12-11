de253c3f945bca28ce962d5091d70c88bf685295
jasonkaminer <jasonkaminer@gmail.com>
2019-12-11 18:45:10 +1300

    Added ViewPager to move between current job tabs. I changed the coordinator layout in the main_activity.xml back to a frame layout because I was having some issues. Feel free to change it back if it was important

--- app/src/main/java/com/bellyful/bellyfulapp/CurrentJobsFragment.java
+++ app/src/main/java/com/bellyful/bellyfulapp/CurrentJobsFragment.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 /**
@@ -20,14 +26,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CurrentJobsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "pageNum";
+    private static final String ARG_PARAM2 = "pageTitle";
+
+    private int mPageNum;
+    private String mTitle;
 
     private OnFragmentInteractionListener mListener;
 
@@ -35,20 +40,29 @@ public class CurrentJobsFragment extends Fragment {
         // Required empty public constructor
     }
 
+//    public CurrentJobsFragment(int page, String title) {
+//        CurrentJobsFragment currentJobsFragment = new CurrentJobsFragment();
+//        Bundle args = new Bundle();
+//        args.putInt("someInt", page);
+//        args.putString("someTitle", title);
+//        currentJobsFragment.setArguments(args);
+//        return currentJobsFragment;
+//    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param page Parameter 1.
+     * @param title Parameter 2.
      * @return A new instance of fragment CurrentJobsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CurrentJobsFragment newInstance(String param1, String param2) {
+    public static CurrentJobsFragment newInstance(int page, String title) {
         CurrentJobsFragment fragment = new CurrentJobsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM1, page);
+        args.putString(ARG_PARAM2, title);
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,14 +71,19 @@ public class CurrentJobsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mPageNum = getArguments().getInt(ARG_PARAM1);
+            mTitle = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_current_jobs, container, false);
+        ViewPager viewPager = (ViewPager) root.findViewById(R.id.currentJobsViewPager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_current_jobs, container, false);
     }
@@ -108,4 +127,19 @@ public class CurrentJobsFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+//    //This sets up the viewPager controlling the top tabs
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        ViewPager viewPager = view.findViewById(R.id.currentJobsViewPager);
+//        TabLayout tabLayout = view.findViewById(R.id.currentJobTabLayout);
+//
+//        //Attach tabLayout to viewPager
+//        tabLayout.setupWithViewPager(viewPager);
+//
+//        ViewPagerAdapter viewPagerAdapter= new ViewPagerAdapter(getChildFragmentManager());
+//
+//
+//    }
 }
